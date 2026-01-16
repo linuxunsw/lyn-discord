4eafcbf8709436c1bd16d57c4fc779a2fac855dd
isobelmcrae <isobel@isobel.zip>
2026-01-17 03:44:08 +1100

    chore: delete commonjs

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -2,30 +2,37 @@ import "dotenv/config";
 import { REST, Routes } from "discord.js";
 import fs from "node:fs";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-const commands = [];
-// Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
-for (const folder of commandFolders) {
-  // Grab all the command files from the commands directory you created earlier
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".ts"));
-  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      commands.push(command.data.toJSON());
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-      );
+async function loadCommands() {
+  const commands = [];
+  // Grab all the command folders from the commands directory you created earlier
+  const foldersPath = path.join(dirname, "commands");
+  const commandFolders = fs.readdirSync(foldersPath);
+
+  for (const folder of commandFolders) {
+    // Grab all the command files from the commands directory you created earlier
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs
+      .readdirSync(commandsPath)
+      .filter((file) => file.endsWith(".ts") || file.endsWith(".js"));
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const commandModule = await import(filePath);
+      const command = commandModule.default;
+      if (command && "data" in command && "execute" in command) {
+        commands.push(command.data.toJSON());
+      } else {
+        console.log(
+          `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+        );
+      }
     }
   }
+  return commands;
 }
 
 // Construct and prepare an instance of the REST module
@@ -34,6 +41,8 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN as string);
 // and deploy your commands!
 (async () => {
   try {
+    const commands = await loadCommands();
+
     console.log(
       `Started refreshing ${commands.length} application (/) commands.`,
     );
