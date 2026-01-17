c00b6242bbf317a6a0ff00d254cb7ca04a0229e5
isobelmcrae <isobel@isobel.zip>
2026-01-17 22:04:18 +1100

    feat(mail): use amazon ses

--- src/commands/verify/mail.tsx
+++ src/commands/verify/mail.tsx
@@ -1,10 +1,24 @@
 import { createTransport } from "nodemailer";
 import { OTPEmail } from "./templates/otp";
-import { verifyMailSubject, zIDEmail } from "../../config";
+import { verifyEmail, verifyMailSubject, zIDEmail } from "../../config";
+import { SESv2Client, SendEmailCommand } from "@aws-sdk/client-sesv2";
 
-const SEND_MAIL = process.env.VERIFY_EMAIL;
+const region = process.env.AWS_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+if (!region || !accessKeyId || !secretAccessKey) {
+  throw new Error("Missing AWS environment variables");
+}
+
+const sesClient = new SESv2Client({
+  region,
+  credentials: {
+    accessKeyId,
+    secretAccessKey,
+  },
+});
 const transporter = createTransport({
-  jsonTransport: true,
+  SES: { sesClient, SendEmailCommand },
 });
 
 export async function sendOTPMail(otp: string, zID: string) {
@@ -12,14 +26,13 @@ export async function sendOTPMail(otp: string, zID: string) {
 
   transporter.sendMail(
     {
-      from: SEND_MAIL,
+      from: verifyEmail,
       to: zIDEmail(zID),
       subject: verifyMailSubject,
       html: emailContent,
     },
-    (err, info) => {
+    (err) => {
       if (err) throw err;
-      console.log(JSON.parse(info.message));
     },
   );
 
